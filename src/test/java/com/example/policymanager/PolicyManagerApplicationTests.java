07bfbcd63e43165a36eb8fb801e0aff87b9238a4
addy2510 <adwaittiwari25@gmail.com>
2026-01-18 12:34:38 +0530

    Initial commit

---
+++ src/test/java/com/example/policymanager/PolicyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.policymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
