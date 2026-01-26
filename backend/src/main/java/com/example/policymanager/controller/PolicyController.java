1f14a4242281ea0fc3bcfe33ca42dca03a39ed36
addy2510 <adwaittiwari25@gmail.com>
2026-01-26 12:13:15 +0530

    removed useless imports

--- backend/src/main/java/com/example/policymanager/controller/PolicyController.java
+++ backend/src/main/java/com/example/policymanager/controller/PolicyController.java
@@ -9,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/policy")
 public class PolicyController {
